f88a3e4599c927e280bc2bd10bc1434daa3c755d
aminul-islam-sajid <mdaminulislamsajid@gmail.com>
2023-12-11 01:44:21 +0600

    [Done] our Card

--- src/components/OurService/OurService.jsx
+++ src/components/OurService/OurService.jsx
@@ -1,13 +1,21 @@
+import { Icon } from '@iconify/react';
 import React from 'react';
 
 const OurService = () => {
-    return (
-        <div className='text-center'>
-                  <h1 className=' mt-24 text-5xl font-bold'>Our services</h1>
-            <hr/>
-            <p className='text-lg my-2'>Tech firms provide software development, web/mobile apps, IT consulting, cloud services, cybersecurity,  data analytics, <br /> UI/UX design, and hardware solutions. They develop software, manage systems, ensure security, <br /> analyze data, and offer design and consulting services.</p>
-        </div>
-    );
+	return (
+		<div className="text-center">
+			<h1 className=" mt-24 text-5xl font-bold">Our services</h1>
+			<div className="flex justify-center text-lightSecondary">
+				<Icon icon="pepicons-pop:line-x" />
+			</div>
+			<p className="text-lg my-10">
+				Tech firms provide software development, web/mobile apps, IT consulting,
+				cloud services, cybersecurity, data analytics, <br /> UI/UX design, and
+				hardware solutions. They develop software, manage systems, ensure
+				security, <br /> analyze data, and offer design and consulting services.
+			</p>
+		</div>
+	);
 };
 
 export default OurService;
--- src/components/OurService/ServiceCard.jsx
+++ src/components/OurService/ServiceCard.jsx
@@ -1,31 +1,75 @@
+import { Icon } from '@iconify/react';
 import React from 'react';
 
 const ServiceCard = () => {
-    return (
-        <div className='flex justify-center gap-7'>
-            {/* first card (always active) */}
-            <div className='bg-[#07075d] w-80 h-80 border-solid border-2 border-lightSecondary text-white'>
-                <div className='text-center'>
-                    <div>
-                            <img src="" alt="" />
-                        <h1>Design</h1>
-                        <p> dolor sit amet consectetur adipisicing <br /> elit. Nostrum?
-                        </p>
-                        <p></p>
-                    </div>
-                </div>
-            </div>
-            {/* second card (active on hover) */}
-            <div className='hover:bg-primaryColor  w-80 h-80 border-solid border-2 border-lightSecondary hover:text-white'>
-                <h2>hallo</h2>
-            </div>
-            {/* third card (active on hover) */}
-            <div className='hover:bg-primaryColor  w-80 h-80 border-solid border-2 border-lightSecondary hover:text-white'>
-                <h2>hallo</h2>
-
-            </div>
-        </div>
-    );
+	return (
+		<div className="flex justify-center gap-7">
+			{/* first card (always active) */}
+			<div className="bg-[#07075d] w-80 h-80  text-white flex items-center justify-center">
+				<div className="text-center">
+					<div>
+						<div className="flex justify-center items-center">
+							<div className="bg-gradient-to-r from-secondaryColor to-lightSecondary rounded-full w-16 h-16 flex justify-center items-center">
+								<Icon
+									className="hover:animate-ping"
+									icon="fluent-emoji-high-contrast:artist-palette"
+									hFlip={true}
+								/>
+							</div>
+						</div>
+						<h1 className="text-2xl font-bold my-4">Design</h1>
+						<p>
+							{' '}
+							dolor sit amet consectetur adipisicing <br /> elit. Nostrum?
+						</p>
+						<p></p>
+					</div>
+				</div>
+			</div>
+			{/* second card (active on hover) */}
+			<div className="hover:bg-primaryColor w-80 h-80  hover:text-white text-dark flex items-center justify-center">
+				<div className="text-center">
+					<div>
+						<div className="flex justify-center items-center">
+							<div className="bg-gradient-to-r from-secondaryColor to-lightSecondary rounded-full w-16 h-16 flex justify-center items-center">
+								<Icon
+									className="hover:animate-ping"
+									icon="fluent-emoji-high-contrast:artist-palette"
+									hFlip={true}
+								/>
+							</div>
+						</div>
+						<h1 className="text-2xl font-bold my-4">Development</h1>
+						<p className="my-2">
+							dolor sit amet consectetur adipisicing <br /> elit. Nostrum?
+						</p>
+						<p></p>
+					</div>
+				</div>
+			</div>
+			{/* third card (active on hover) */}
+			<div className="hover:bg-primaryColor w-80 h-80  hover:text-white text-dark flex items-center justify-center">
+				<div className="text-center">
+					<div>
+						<div className="flex justify-center items-center">
+							<div className="bg-gradient-to-r from-secondaryColor to-lightSecondary rounded-full w-16 h-16 flex justify-center items-center">
+								<Icon
+									className="hover:animate-ping"
+									icon="fluent-emoji-high-contrast:artist-palette"
+									hFlip={true}
+								/>
+							</div>
+						</div>
+						<h1 className="text-2xl font-bold my-4">Marketing</h1>
+						<p className="my-2">
+							dolor sit amet consectetur adipisicing <br /> elit. Nostrum?
+						</p>
+						<p></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ServiceCard;
