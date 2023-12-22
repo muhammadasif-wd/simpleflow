edc95af829f6c9de0366866fa0286034bb21d0e3
muhammadasif-wd <muhammadasif.wd@gmail.com>
2023-12-22 11:29:41 +0600

    [FEAT] Introduce [Frontend Initial DONE]

--- src/components/header/Footer.jsx
+++ src/components/header/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
 		<footer className="bg-primaryColor w-full">
 			<section style={{ backgroundImage: `url(${images.map})`, backgroundRepeat: 'no-repeat' }} className="bg-cover">
 				<div className="w-11/12 mx-auto pt-10 flex flex-col justify-center items-center">
-					{footer_items.map(({ _id, followUsMessage, aboutCompany }) => (
+					{footer_items.map(({ _id, followUsMessage, aboutCompany, facebookURL, contactNumber, linkedinURL, location, locationURL, twitterURL, youtubeURL }) => (
 						<div key={_id} className="mx-auto w-full max-w-screen-xl">
 							<div className="grid gap-10 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mx-auto text-white">
 								<article>
@@ -22,10 +22,10 @@ const Footer = () => {
 									<div className="text-sm font-medium">
 										<p className="mt-8 text-justify">{followUsMessage}</p>
 										<div className="flex items-center gap-3 mt-3">
-											<Link className='text-white' href='/'><Icon className="w-6 h-6" icon="bi:facebook" /></Link>
-											<Link className='text-white' href='/'><Icon className="w-8 h-8" icon="bi:youtube" /></Link>
-											<Link className='text-white' href='/'><Icon className="w-6 h-6" icon="bi:twitter-x" /></Link>
-											<Link className='text-white' href='/'><Icon className="w-6 h-6" icon="bi:linkedin" /></Link>
+											<Link target='_blank' className='text-white' href={facebookURL}><Icon className="w-6 h-6" icon="bi:facebook" /></Link>
+											<Link target='_blank' className='text-white' href={youtubeURL}><Icon className="w-8 h-8" icon="bi:youtube" /></Link>
+											<Link target='_blank' className='text-white' href={twitterURL}><Icon className="w-6 h-6" icon="bi:twitter-x" /></Link>
+											<Link target='_blank' className='text-white' href={linkedinURL}><Icon className="w-6 h-6" icon="bi:linkedin" /></Link>
 										</div>
 									</div>
 								</article>
@@ -34,11 +34,11 @@ const Footer = () => {
 									<div className='mt-8'>
 										<article className="text-white gap-3 flex items-center justify-start my-3">
 											<Icon className="w-8 h-8" icon="mdi:location" />
-											<Link className='text-white' href="/">Example location 1/2A Block-B</Link>
+											<Link className='text-white' href={locationURL}>{location}</Link>
 										</article>
 										<article className="text-white gap-3 flex items-center justify-start ml-3">
 											<Icon className="w-6 h-6" icon="bi:telephone-fill" />
-											<p className="text-start">+88 01285462158</p>
+											<p className="text-start">{contactNumber}</p>
 										</article>
 									</div>
 								</article>
