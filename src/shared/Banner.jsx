cd6cf77f986208efcd1efe13602dc6448cf399c0
Sayem-hussain-majhi <hafejmdsayem@gmail.com>
2024-03-26 22:01:22 +0600

    spa c e

--- src/shared/Banner.jsx
+++ src/shared/Banner.jsx
@@ -10,7 +10,6 @@ const Banner = ({ title }) => {
             <div className='mt-11'>
                 <div
 
-                
                     style={{ backgroundImage: `url(${images.banner})`, backgroundRepeat: 'no-repeat' }} className={`max-w-full text-white`}>
                     <Title title={`${title}`} styles={'3xl:h-[35rem] 2xl:h-[30rem] xl:h-[28rem] lg:h-[25rem] md:h-[20rem] h-[15rem] flex justify-center items-center text-white font-bold'} />
                 </div>
